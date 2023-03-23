2e2b33e6090401de96a2bf5c78a42f9bef3b8726
lllod <ygo.admin@gmail.com>
2023-03-23 15:00:18 +0400

    add seminar3 task2

--- seminar3/task2.py
+++ seminar3/task2.py
@@ -8,3 +8,38 @@
     6
     -> 5
 """
+from random import randint as rand
+
+
+def list_create() -> tuple:
+    print('Введите длину списка:', end=' ')
+    list_length = num_isdigit()
+    nums_list = [rand(0, list_length) for i in range(list_length)]
+    print(f'Список чисел: {nums_list}')
+    return nums_list, list_length
+
+
+def num_isdigit() -> int:
+    while True:
+        num = input()
+        if num.isdigit() and int(num) > 0:
+            return int(num)
+        print('Необходимо ввести целое положительное число!')
+
+
+def num_modul() -> int:
+    print('Введите число:', end=' ')
+    user_num = num_isdigit()
+    num_list, max_modul = list_create()
+    num_modul = 0
+    for i in num_list:
+        if user_num - i == 0:
+            return user_num
+        elif abs(user_num - i) < max_modul:
+            max_modul = abs(user_num - i)
+            num_modul = i
+    return num_modul
+
+
+if __name__ == '__main__':
+    print(num_modul())
