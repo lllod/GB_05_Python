f94b0e73083f0464504a9e34a345027da35a3135
lllod <ygo.admin@gmail.com>
2023-03-23 16:54:22 +0400

    add seminar3 task5

--- seminar3/task5.py
+++ seminar3/task5.py
@@ -5,3 +5,11 @@
 Код print(query({'course': 'python', 'lesson': 2, 'challenge': 17})) должен возвращать строку:
 challenge=17&course=python&lesson=2
 """
+
+# in_dict = {'course': 'python', 'lesson': 2, 'challenge': 17}
+# sort_in_dict = sorted(in_dict.items(), key=lambda x: x[0])
+# print(sort_in_dict)
+# print(sort_in_dict[0][1])
+# for i in sort_in_dict:
+#     text = '&'.join('='.join(str(_)) for _ in i)
+# print(text)
