3ddeaf9e755ebd7d52b5fd23e679e0ae608503f7
lllod <ygo.admin@gmail.com>
2023-03-23 16:03:48 +0400

    add seminar3 task4

--- seminar3/task4.py
+++ seminar3/task4.py
@@ -7,3 +7,14 @@
 Пример вывода:
 YES
 """
+from collections import Counter
+
+
+def is_anagrams() -> str:
+    first_words = list(''.join(i.upper() for i in input('Введите первую фразу: ') if i.isalpha()))
+    second_words = list(''.join(i.upper() for i in input('Введите вторую фразу: ') if i.isalpha()))
+    return 'YES' if Counter(first_words) == Counter(second_words) else 'NO'
+
+
+if __name__ == '__main__':
+    print(is_anagrams())
