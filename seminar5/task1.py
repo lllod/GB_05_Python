ae39d4618ff90aece3ba3a385a9e416b88d037b3
lllod <ygo.admin@gmail.com>
2023-03-30 16:48:29 +0400

    add seminar5 task1

---
+++ seminar5/task1.py
@@ -0,0 +1,16 @@
+"""
+Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+*Пример:*
+A = 3; B = 5 -> 243 (3⁵)
+    A = 2; B = 3 -> 8
+"""
+
+
+def exponentiation(number: int, num_to_exp: int) -> int:
+    return number * (exponentiation(number, num_to_exp - 1) if num_to_exp > 2 else number)
+
+
+if __name__ == '__main__':
+    num = int(input('Введите число: '))
+    to_exp = int(input('Введите степень числа: '))
+    print(exponentiation(num, to_exp))
